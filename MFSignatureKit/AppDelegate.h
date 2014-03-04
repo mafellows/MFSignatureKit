9c7de404acbe95a6da8c43ddff54dc6b9a645c5e
Michael Fellows <michael.fellows12@gmail.com>
2014-03-04 09:49:13 -0500

    Initial Commit

---
+++ MFSignatureKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MFSignatureKit
+//
+//  Created by Michael Fellows on 3/4/14.
+//  Copyright (c) 2014 Michael Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MFSignatureKitTests/MFSignatureKitTests.m
@@ -0,0 +1,34 @@
+//
+//  MFSignatureKitTests.m
+//  MFSignatureKitTests
+//
+//  Created by Michael Fellows on 3/4/14.
+//  Copyright (c) 2014 Michael Fellows. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MFSignatureKitTests : XCTestCase
+
+@end
+
+@implementation MFSignatureKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
